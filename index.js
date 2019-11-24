108a696812ba00cda2a34656533d42044f2e501d
ctman96 <ctman96@students.cs.ubc.ca>
2019-11-24 11:25:21 -0800

    Take directory argument, find all ts files in directory

--- index.js
+++ index.js
@@ -1,10 +1,43 @@
-const Parser = require('typescript-parser');
+'use strict';
+// TODO require Matteo's parser
+const fs = require('fs');
 
-async function test() {
-	const parser = new Parser.TypescriptParser();
-	const test = await parser.parseFile('test.ts', 'workspace root');
-	console.log(test);
-	console.log(test.declarations[0].methods);
+if (process.argv.length != 3) {
+	console.log('Invalid arguments! call with \'node index.js ${absolute path}\'');
+	process.exit(1);
+}
+const dir = process.argv[2];
+
+if (!fs.existsSync(dir) || !fs.lstatSync(dir).isDirectory()) {
+	console.log(`${dir} is not a valid directory!`);
+	process.exit(1);
 }
 
-test();
+console.log(`Analyzing ${dir}`)
+
+const getAllFiles = function (directory) {
+	const files = [];
+	fs.readdirSync(`${directory}/`).forEach((file) => {
+		// If directory, recurse
+		if (fs.lstatSync(`${directory}/${file}`).isDirectory()) {
+			files.concat(getAllFiles(`${directory}/${file}`));
+		} else {
+			// Add any ts files
+			if (file.match(/\.ts$/) !== null) {
+				files.push(`${directory}/${file}`);
+			}
+		}
+	})
+	return files;
+}
+
+const main = async function () {
+	const files = getAllFiles(dir)
+	
+	files.forEach((file) => {
+		console.log(`Parsing ${file}`);
+		const node = {}// TODO call Matteo's parser
+	})
+}
+
+main();
