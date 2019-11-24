cabef168a8c23a5eb749ca560721066c69e38427
ctman96 <ctman96@students.cs.ubc.ca>
2019-11-24 11:33:09 -0800

    Get commit details

--- index.js
+++ index.js
@@ -32,12 +32,53 @@ const getAllFiles = function (directory) {
 }
 
 const main = async function () {
-	const files = getAllFiles(dir)
+	const commit = await new Promise((resolve, reject) => {
+		require('child_process').exec('git rev-parse --short=7 HEAD', function(err, stdout) {
+			if (err || !stdout) {
+				console.log(err);
+				reject(err);
+			} else {
+				resolve(stdout.replace(/(\r\n|\n|\r)/gm, "")); // Clear line breaks
+			}
+		});
+	});
+	console.log(`Last commit hash on this branch is: ${commit}`);
+
+	const datetimestr = await new Promise((resolve, reject) => {
+		require('child_process').exec(`git show --no-patch --no-notes --pretty=\'%cd\' ${commit}`, function(err, stdout) {
+			if (err) {
+				console.log(err);
+				reject(err);
+			} else {
+				resolve(stdout);
+			}
+		});
+	});
+	console.log(`Commit datetime is: ${datetimestr}`);
+
+	const datetime = new Date(datetimestr).toISOString();
+	console.log(`Commit date is: ${datetime}`);
+
+	const files = getAllFiles(dir);
 	
+	const nodes = [];
+	const links = [];
 	files.forEach((file) => {
 		console.log(`Parsing ${file}`);
-		const node = {}// TODO call Matteo's parser
-	})
+		const parsed = {}// TODO call Matteo's parser
+		nodes.concat(parsed.node);
+		links.concat(parsed.links);
+	});
+
+	const out = {
+		commit,
+		datetime,
+		nodes,
+		links,
+	}
+
+	console.log(out);
+	// TODO output?
 }
 
 main();
